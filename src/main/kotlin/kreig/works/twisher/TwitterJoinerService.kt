f8a11473b1ef9900ad5b718a750b4e4abf45c943
Erik Giron <1243595+kreigiron@users.noreply.github.com>
2019-05-25 16:20:03 -0600

    Fix wiring

--- src/main/kotlin/kreig/works/twisher/TwitterJoinerService.kt
+++ src/main/kotlin/kreig/works/twisher/TwitterJoinerService.kt
@@ -5,8 +5,16 @@ import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 
 @ApplicationScoped
-open class TwitterJoinerService @Inject constructor(private val twitterClient: TwitterClient) : JoinerService {
+open class TwitterJoinerService : JoinerService {
+    @Inject lateinit var twitterClient: TwitterClient
 
+    constructor() {
+
+    }
+
+    constructor(twitterClient: TwitterClient) {
+        this.twitterClient = twitterClient
+    }
     override fun join(url: String) : String {
         return twitterClient.getTwits(url).joinToString("\n")
     }
